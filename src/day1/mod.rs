369c2ffcb2a87e63746423920228f1c4d181301f
Speykious <speykious@gmail.com>
2020-12-02 03:19:33 +0100

    Hehehe, finished the first puzzles

---
+++ src/day1/mod.rs
@@ -0,0 +1,50 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader, Result};
+
+pub fn main() -> Result<()> {
+  let file = File::open("puzzle-input.dat")?;
+  let lines = BufReader::new(file).lines();
+  let mut numbers: Vec<u64> = Vec::new();
+  for line in lines {
+    numbers.push(line?.parse().unwrap());
+  }
+  println!("Finding two numbers summing up to 2020...");
+  let (a, b) = puzzle1(&numbers);
+  println!("Found {} + {} == {}, summing up to {}", a, b, a + b, a * b);
+  println!("Finding three numbers summing up to 2020...");
+  let (a, b, c) = puzzle2(&numbers);
+  println!("Found {} + {} + {} == {}, summing up to {}", a, b, c, a + b + c, a * b * c);
+
+  Ok(())
+}
+
+fn puzzle1(xs: &Vec<u64>) -> (u64, u64) {
+  let (mut a, mut b);
+  for j in 0..(xs.len() - 1) {
+    a = xs[j];
+    for i in (j+1)..xs.len() {
+      b = xs[i];
+      if a + b == 2020 {
+        return (a, b);
+      }
+    }
+  }
+  (0, 0)
+}
+
+fn puzzle2(xs: &Vec<u64>) -> (u64, u64, u64) {
+  let (mut a, mut b, mut c);
+  for k in 0..(xs.len() - 2) {
+    a = xs[k];
+    for j in (k+1)..(xs.len() - 1) {
+      b = xs[j];
+      for i in (j+1)..xs.len() {
+        c = xs[i];
+        if a + b + c == 2020 {
+          return (a, b, c);
+        }
+      }
+    }
+  }
+  (0, 0, 0)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
-fn main() {
+use std::io::Result;
+mod day1;
+
+fn main() -> Result<()> {
   println!("Hello, world!");
+  day1::main() 
 }
