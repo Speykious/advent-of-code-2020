1817915c3410b763b4094db0272f61d27a25c7f5
Speykious <speykious@gmail.com>
2020-12-09 03:16:58 +0100

    Man, taking the examples to test really pays out

--- src/day8.rs
+++ src/day8.rs
@@ -2,8 +2,8 @@ use std::fs::File;
 use std::io::{self, BufRead, BufReader};
 use std::collections::HashSet;
 
-#[derive(Debug)]
-enum Inst { Nop, Acc(i32), Jmp(i32) }
+#[derive(Debug, Clone, Copy)]
+enum Inst { Nop(i32), Acc(i32), Jmp(i32) }
 
 pub fn main() -> io::Result<()> {
   let file = File::open("resources/day8.dat")?;
@@ -12,33 +12,62 @@ pub fn main() -> io::Result<()> {
     lines.push(line?);
   }
 
-  let instructions: Vec<Inst> = lines.iter()
+  let mut instructions: Vec<Inst> = lines.iter()
   .map(|line| {
     let mut iter = line.split_whitespace();
     let inst = iter.next().unwrap();
     let n = iter.next().unwrap().parse().unwrap();
     match inst {
-      "nop" => Inst::Nop,
+      "nop" => Inst::Nop(n),
       "acc" => Inst::Acc(n),
       "jmp" => Inst::Jmp(n),
       _ => panic!("Panik!! Data corrupted!"),
     }
   }).collect();
   
+  let nop_jmps: Vec<usize> = instructions.iter().enumerate()
+  .filter(|(_, inst)| match inst { Inst::Nop(_) | Inst::Jmp(_) => true, _ => false })
+  .map(|(i, _)| i)
+  .collect();
+
+  for j in 0..nop_jmps.len() {
+    // Had to come up with something clever to not borrow instructions as mutable and immutable
+    { swap_nop_jmp(&mut instructions, nop_jmps[j]); }
+    let (len, i, acc, terminates) = execute(&instructions, false);
+    { swap_nop_jmp(&mut instructions, nop_jmps[j]); }
+    println!("len={:<3} i={:<3} acc={:<4} | terminates={}", len, i, acc, terminates);
+    if terminates { break }
+  }
+  
+  Ok(())
+}
+
+fn swap_nop_jmp(instructions: &mut Vec<Inst>, idx: usize) {
+  let nop_jmp = &mut instructions[idx];
+  *nop_jmp = match nop_jmp {
+    Inst::Nop(n) => Inst::Jmp(*n),
+    Inst::Jmp(n) => Inst::Nop(*n),
+    _ => panic!("Panik!! How am I getting an Inst::Acc?!"),
+  };
+}
+
+fn execute(instructions: &Vec<Inst>, bprint: bool) -> (usize, i32, i32, bool) {
   let mut i: i32 = 0;
   let mut acc: i32 = 0;
   let mut executed: HashSet<i32> = HashSet::new();
-
+  let mut terminates: bool = false;
+  let len: i32 = instructions.len() as i32;
+  
   while executed.insert(i) {
-    print!("(i={} acc={}) => ", i, acc);
+    if bprint { print!("i={:<3} acc={:<4}    => ", i, acc); }
     match instructions[i as usize] {
-      Inst::Nop => { i += 1; print!("nop"); },
-      Inst::Acc(n) => { acc += n; i += 1; print!("acc"); },
-      Inst::Jmp(n) => { i += n; print!("jmp"); },
+      Inst::Nop(_) => { i += 1; if bprint { print!("nop"); } },
+      Inst::Acc(n) => { acc += n; i += 1; if bprint { print!("acc"); } },
+      Inst::Jmp(n) => { i += n; if bprint { print!("jmp"); } },
     }
-    println!(" => (i={}, acc={})", i, acc);
+    if bprint { println!(" =>    i={:<3} acc={}", i, acc); }
+    if i == len { terminates = true; break; }
   }
 
-  println!("\nFinal value of the accumulator: {}", acc);
-  Ok(())
+  (executed.len(), i, acc, terminates)
 }
