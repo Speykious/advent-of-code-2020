fff001a571f38d1640a04c9c077c432909d33ed6
Speykious <speykious@gmail.com>
2020-12-09 14:40:47 +0100

    That's not the right answer, because I DIDN'T READ IT PROPERLY

--- src/day9.rs
+++ src/day9.rs
@@ -2,13 +2,34 @@ use std::fs::File;
 use std::io::{self, BufRead, BufReader};
 
 pub fn main() -> io::Result<()> {
-  let file = File::open("resources/day8.dat")?;
-  let mut lines = Vec::new();
+  let file = File::open("resources/day9.dat")?;
+  let mut numbers: Vec<u64> = Vec::new();
   for line in BufReader::new(file).lines() {
-    lines.push(line?);
+    numbers.push(line?.parse().unwrap());
+  }
+  
+  let preamble: Vec<u64> = numbers[..25].to_vec();
+  let numbers = numbers[25..].to_vec();
+  for n in numbers {
+    if !is_sum_of_2_in(n, &preamble) {
+      println!("The answer is {}", n);
+      break;
+    }
   }
 
-  
-
   Ok(())
+}
+
+fn is_sum_of_2_in(n: u64, v: &Vec<u64>) -> bool {
+  let vlen = v.len();
+  for i in 0..vlen-1 {
+    let a = v[i];
+    for j in i+1..vlen {
+      if a + v[j] == n {
+        return true;
+      }
+    }
+  }
+
+  false
 }
