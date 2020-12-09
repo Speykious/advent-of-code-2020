53fe4cac9afcca578525ee87824c66f3c1fb59e5
Speykious <speykious@gmail.com>
2020-12-09 01:38:37 +0100

    Got data in a nice vec of enum

--- src/day8.rs
+++ src/day8.rs
@@ -1,5 +1,9 @@
 use std::fs::File;
 use std::io::{self, BufRead, BufReader};
+use std::collections::HashSet;
+
+#[derive(Debug)]
+enum Inst { Nop, Acc(i32), Jmp(i32) }
 
 pub fn main() -> io::Result<()> {
   let file = File::open("resources/day8.dat")?;
@@ -8,5 +12,21 @@ pub fn main() -> io::Result<()> {
     lines.push(line?);
   }
 
+  let data: Vec<Inst> = lines.iter()
+  .map(|line| {
+    let mut iter = line.split_whitespace();
+    let inst = iter.next().unwrap();
+    let n = iter.next().unwrap().parse().unwrap();
+    match inst {
+      "nop" => Inst::Nop,
+      "acc" => Inst::Acc(n),
+      "jmp" => Inst::Jmp(n),
+      _ => panic!("Panik!! Data corrupted!"),
+    }
+  }).collect();
+  
+  
+
+  println!("{:?}", data);
   Ok(())
 }
