682976b65eccedac39693c04391ff5752a2d5b09
Speykious <speykious@gmail.com>
2020-12-13 19:00:24 +0100

    Looks like I found the problem in day11 in Rust

--- src/day11.rs
+++ src/day11.rs
@@ -40,10 +40,31 @@ fn val(c: Option<&Cell>) -> u32 {
 fn neighbors1(grid: &Grid, x: usize, y: usize) -> u32 {
   let mut counter = 0;
   let (x, y) = (x as isize, y as isize);
-  for j in y - 1 .. y + 1 {
-    for i in x - 1 .. x + 1 {
-      if j == y && i == x { continue }
-      counter += val(cell(grid, x + i, y + j));
+  for j in y - 1 ..= y + 1 {
+    for i in x - 1 ..= x + 1 {
+      if (i, j) == (x, y) { continue }
+      let cellthing = cell(grid, i, j);
+      counter += val(cellthing);
+    }
+  }
+  counter
+}
+
+fn neighbors2(grid: &Grid, x: usize, y: usize) -> u32 {
+  let mut counter = 0;
+  let (x, y) = (x as isize, y as isize);
+  for j in y - 1 ..= y + 1 {
+    for i in x - 1 ..= x + 1 {
+      if (i, j) == (x, y) { continue }
+      let mut d = 1;
+      loop {
+        let cellthing = cell(grid, i*d, j*d);
+        counter += val(cellthing);
+        match cellthing {
+          Some(&Cell::NoSeat) => d += 1,
+          _ => break,
+        }
+      }
     }
   }
   counter
@@ -58,14 +79,11 @@ fn automata(grid: &mut Grid) -> (u32, bool) {
   for j in 0 .. newgrid.len() {
     let newline = &mut newgrid[j];
     for i in 0 .. newline.len() {
-      match newline[i] {
-        Cell::NoSeat => (),
-        _ => {
-          let neighbors = neighbors1(grid, i, j);
-          if neighbors >= 4 { newline[i] = Cell::Empty; }
-          else if neighbors == 0 { newline[i] = Cell::Full; }
-        },
-      }
+      if newline[i] == Cell::NoSeat { continue }
+      //let neighbors = neighbors1(grid, i, j);
+      let neighbors = neighbors2(grid, i, j);
+      if neighbors >= 4 { newline[i] = Cell::Empty; }
+      else if neighbors == 0 { newline[i] = Cell::Full; }
     }
   }
   let (mut seats, mut changed) = (0, false);
@@ -102,6 +120,7 @@ pub fn main() -> io::Result<()> {
     print_grid(&grid);
     println!("{} seats, {}running", s, if r { "" } else { "not " });
     running = r; seats = s;
+    std::thread::sleep_ms(64);
   }
 
   Ok(())
